fab09710a3324ca941b1156784e63276f37b846d
seipan <yamasakipann0218@gmail.com>
2022-07-21 21:52:06 +0900

    バチャ追加分

---
+++ ABC_practice/ABC236-C.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+#include <unordered_set>
+
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+const int inf = INT_MAX / 2; 
+const ll infl = 1LL << 60;
+typedef pair<ll,ll> pi;
+#define ALL(a)  (a).begin(),(a).end()
+#define mod 1048576
+
+const double PI  = acos(-1);
+
+#define YesNo(bool) if(bool){cout<<"Yes"<<endl;}else{cout<<"No"<<endl;}
+
+inline ll in_ll() {ll x; cin >> x; return x;}
+inline double in_double() {{double x; cin >> x; return x;}}
+inline string in_str() {string x; cin >> x; return x;}
+inline int ctoi(char c) {return c - '0';}
+
+template <typename T> inline bool chmin(T& a, const T& b) {bool compare = a > b; if (a > b) a = b; return compare;}
+template <typename T> inline bool chmax(T& a, const T& b) {bool compare = a < b; if (a < b) a = b; return compare;}
+
+using Graph = vector<vector<int>>;
+
+//高速累乗
+ll powpow(ll x, ll n) {
+  if (n == 0) return 1;
+  ll val = powpow(x, n / 2);
+  val *= val;
+  if (n % 2 == 1) val *= x;
+  return val;
+}
+
+const ld eps = 1.0e-18;
+//精度のいいsqrt
+ld mysqrtl(ld x)
+{
+  ld a = sqrt((double)x); 
+  do {
+    a = (a + x/a) / 2.0L;
+  } while (fabsl(x - a*a) > eps);
+  return a;
+}
+
+
+int main(){
+    ll N,M;cin >> N >> M;
+    vector<string> S(N);
+    vector<string> T(M);
+    for(ll i=0;i<N;i++) cin >> S[i];
+    for(ll i=0;i<M;i++) cin >> T[i];
+
+    map<string, int> mp;
+	for(int i=0;i<M;i++){
+		mp[T[i]] = 1;
+	}
+	for(int i=0;i<N;i++){
+		if(mp[S[i]] == 1) cout << "Yes" << endl;
+		else cout << "No" << endl;
+	}
+}
